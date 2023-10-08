76df4f843ad11656bd3bca07f734c62c9981c04a
Diego Cardenas <diego.cardenast@gmail.com>
2023-10-08 16:07:56 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "subtract"){
+        displaySubtractQuestion(num1, num2); 
     } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1, num2);
     } else {
@@ -72,7 +74,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else if (operator === "x") {
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    }else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
@@ -104,8 +108,12 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+    
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
